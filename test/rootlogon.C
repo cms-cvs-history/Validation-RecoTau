e6c119ea6bd3d9cdf056504471e9e61c4b4641f3
Simone Gennai <sha1-c06466410f2ad16966d114488822880c0effa70e@cern.ch>
2008-10-01 09:08:26 +0000

    *** empty log message ***

--- test/rootlogon.C
+++ test/rootlogon.C
@@ -2,7 +2,13 @@
 char *hosttype = gSystem->Getenv( "HOSTTYPE" );
 char *rootsys  = gSystem->Getenv( "ROOTSYS" );
 
+
 // gROOT->Reset();                // Reseting ROOT
+gROOT->LoadMacro("drawEfficiency.C++");
+gROOT->LoadMacro("drawHistograms.C++");
+gROOT->LoadMacro("drawEfficiencyOther.C++");
+gROOT->LoadMacro("drawEfficiencies.C++");
+ 
 gROOT->LoadMacro("tdrstyle.C");
 setTDRStyle();
 printf( "libraries loaded\n" );
