837b98bf135b3df32ab8e6ba17b0eb1b62e962b1
Ricardo Vasquez Sierra <sha1-f7e1e97cb11665c6bb054336d8a4a436897d2a02@cern.ch>
2008-11-27 11:28:10 +0000

    Config file used to make the plots for validation

---
+++ test/makeplots_cfg.py
@@ -0,0 +1,18 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('EWKTauMuon')
+
+process.load('Validation.RecoTau.plotterTauTagValidation_cff')
+
+process.DQMStore = cms.Service("DQMStore")
+
+process.maxEvents = cms.untracked.PSet(            
+    input = cms.untracked.int32(0)         
+)
+
+process.source = cms.Source("EmptySource")
+
+process.p = cms.Path( process.loadTau
+                     +process.plotPFTauEfficiencies
+                     +process.plotPFTauHighEfficiencyEfficiencies
+                     +process.plotCaloTauEfficiencies)
