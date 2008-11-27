675d488f8e956af9406df882b3f90605c83f65be
Ricardo Vasquez Sierra <sha1-f7e1e97cb11665c6bb054336d8a4a436897d2a02@cern.ch>
2008-11-27 11:36:34 +0000

    New config files that include the new plotter feature

--- test/TauTagValidation_cfg.py
+++ test/TauTagValidation_cfg.py
@@ -11,10 +11,6 @@ process.load("HLTriggerOffline.Tau.Validation.HLTTauReferences_cfi")
 
 process.load("Validation/RecoTau/TauTagValidation_cfi")
 
-process.load("Configuration.StandardSequences.GeometryPilot2_cff")
-process.load("Configuration.StandardSequences.MagneticField_cff")
-process.load("Configuration.StandardSequences.Reconstruction_cff")
-
 # Conditions: fake or frontier
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 process.GlobalTag.globaltag = 'IDEAL_V9::All'
@@ -26,30 +22,15 @@ process.DQMStore = cms.Service("DQMStore")
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-     '/store/relval/CMSSW_2_1_10/RelValZTT/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/IDEAL_V9_v2/0000/08129034-FD99-DD11-BEED-000423D8F63C.root',
-        '/store/relval/CMSSW_2_1_10/RelValZTT/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/IDEAL_V9_v2/0000/3E09F7AB-A699-DD11-B642-000423D94C68.root',
-        '/store/relval/CMSSW_2_1_10/RelValZTT/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/IDEAL_V9_v2/0000/4A07D9D9-AA99-DD11-B646-000423D98DD4.root',
-        '/store/relval/CMSSW_2_1_10/RelValZTT/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/IDEAL_V9_v2/0000/54FE0387-A999-DD11-8ACE-000423D998BA.root',
-        '/store/relval/CMSSW_2_1_10/RelValZTT/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/IDEAL_V9_v2/0000/58EFF32A-A899-DD11-BBD1-000423D991F0.root',
-        '/store/relval/CMSSW_2_1_10/RelValZTT/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/IDEAL_V9_v2/0000/647A6A85-A999-DD11-8159-000423D6CA6E.root',
-        '/store/relval/CMSSW_2_1_10/RelValZTT/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/IDEAL_V9_v2/0000/66A0F94F-AA99-DD11-8DAB-000423D94E1C.root',
-        '/store/relval/CMSSW_2_1_10/RelValZTT/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/IDEAL_V9_v2/0000/74AD18D5-A499-DD11-AE7C-000423D996C8.root',
-        '/store/relval/CMSSW_2_1_10/RelValZTT/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/IDEAL_V9_v2/0000/880BA685-A999-DD11-B3E2-001617DBD472.root',
-        '/store/relval/CMSSW_2_1_10/RelValZTT/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/IDEAL_V9_v2/0000/8A4147D3-AA99-DD11-8F6F-000423D99996.root',
-        '/store/relval/CMSSW_2_1_10/RelValZTT/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/IDEAL_V9_v2/0000/9CB09C57-AA99-DD11-B17B-001617C3B64C.root',
-        '/store/relval/CMSSW_2_1_10/RelValZTT/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/IDEAL_V9_v2/0000/A49D4C86-A999-DD11-BF99-001617C3B5F4.root',
-        '/store/relval/CMSSW_2_1_10/RelValZTT/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/IDEAL_V9_v2/0000/CE142AD1-A899-DD11-81F9-000423D98C20.root',
-        '/store/relval/CMSSW_2_1_10/RelValZTT/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/IDEAL_V9_v2/0000/D2BE4560-AA99-DD11-A6A3-001617C3B5F4.root',
-        '/store/relval/CMSSW_2_1_10/RelValZTT/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/IDEAL_V9_v2/0000/DE79C186-AA99-DD11-A31B-001617C3B6FE.root',
-        '/store/relval/CMSSW_2_1_10/RelValZTT/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/IDEAL_V9_v2/0000/E63DE9B5-AA99-DD11-8464-000423D98750.root',
-        '/store/relval/CMSSW_2_1_10/RelValZTT/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/IDEAL_V9_v2/0000/EC09555F-A599-DD11-99C5-001617C3B5F4.root'
+        '/store/relval/CMSSW_2_2_0/RelValZTT/GEN-SIM-RECO/STARTUP_V7_v1/0000/10627D20-34B9-DD11-BAD2-000423D986C4.root',
+        '/store/relval/CMSSW_2_2_0/RelValZTT/GEN-SIM-RECO/STARTUP_V7_v1/0000/84EC5604-37B9-DD11-B0C4-000423D951D4.root',
+        '/store/relval/CMSSW_2_2_0/RelValZTT/GEN-SIM-RECO/STARTUP_V7_v1/0000/B2411152-B5B9-DD11-94D3-000423D6B42C.root',
+        '/store/relval/CMSSW_2_2_0/RelValZTT/GEN-SIM-RECO/STARTUP_V7_v1/0000/B623205B-35B9-DD11-A2A7-000423D991F0.root'
 )
 )
-
-
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(300)
+    input = cms.untracked.int32(-1)
 )
 
 process.SimpleMemoryCheck = cms.Service("SimpleMemoryCheck",
@@ -59,38 +40,14 @@ process.SimpleMemoryCheck = cms.Service("SimpleMemoryCheck",
 process.load("RecoTauTag.Configuration.RecoTauTag_EventContent_cff")
 process.load("Configuration.EventContent.EventContent_cff")
 
-#process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
-#
-#process.printList = cms.EDFilter("ParticleListDrawer",
-#                                 src = cms.InputTag("genParticles"),
-#                                 maxEventsToPrint = cms.untracked.int32(-1)
-#                                 )
-#
-#process.printTree = cms.EDFilter("ParticleTreeDrawer",
-#                                 src = cms.InputTag("genParticles"),
-#                                 printP4 = cms.untracked.bool(False),
-#                                 printPtEtaPhi = cms.untracked.bool(False),
-#                                 printVertex = cms.untracked.bool(False),
-#                                 printStatus = cms.untracked.bool(False),
-#                                 printIndex = cms.untracked.bool(False),
-#                                 status = cms.untracked.vint32(3)
-#)
-
-process.output = cms.OutputModule("PoolOutputModule",
-                                   fileName = cms.untracked.string('letssee.root'),
-                                   outputCommands = cms.untracked.vstring('drop *',
-                                   'keep *_caloRecoTau*_*_*'
-                                   )
-                                  )
+process.load("Validation.RecoTau.RelValHistogramEff_cfi")
                                  
-process.p = cms.Path( process.PFTau
-#                      *process.PFTauHighEfficiency
-                      *process.tautagging
-                      *process.TauMCProducer
-#                      *process.output
-                      *process.CaloTausBothProngs)
-# 		      *process.CaloTausBothProngs
-#    		      *process.PFTausBothProngs)
-#                      *process.PFTausHighEfficiencyBothProngs) 
+process.p = cms.Path(  process.TauMCProducer
+                      *process.CaloTausBothProngs
+    		      *process.PFTausBothProngs
+                      *process.PFTausHighEfficiencyBothProngs
+                      *process.TauEfficiencies
+                      *process.saveTauEff)
+
 
 #process.outpath = cms.EndPath(process.output)
