82e4672f8461e023ede7aa7928c54f7fd4879ca4
Ricardo Vasquez Sierra <sha1-f7e1e97cb11665c6bb054336d8a4a436897d2a02@cern.ch>
2009-01-26 11:06:46 +0000

    Added the file saving tool here to avoid opening up the other file that contained it.

--- test/TauTagValidation_cfg.py
+++ test/TauTagValidation_cfg.py
@@ -4,10 +4,10 @@ process = cms.Process("TEST")
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-        '/store/relval/CMSSW_2_2_0/RelValZTT/GEN-SIM-RECO/STARTUP_V7_v1/0000/10627D20-34B9-DD11-BAD2-000423D986C4.root',
-        '/store/relval/CMSSW_2_2_0/RelValZTT/GEN-SIM-RECO/STARTUP_V7_v1/0000/84EC5604-37B9-DD11-B0C4-000423D951D4.root',
-        '/store/relval/CMSSW_2_2_0/RelValZTT/GEN-SIM-RECO/STARTUP_V7_v1/0000/B2411152-B5B9-DD11-94D3-000423D6B42C.root',
-        '/store/relval/CMSSW_2_2_0/RelValZTT/GEN-SIM-RECO/STARTUP_V7_v1/0000/B623205B-35B9-DD11-A2A7-000423D991F0.root'
+        '/store/relval/CMSSW_3_0_0_pre7/RelValZTT/GEN-SIM-RECO/STARTUP_30X_v1/0006/0C9E3984-57E8-DD11-B89C-001D09F291D2.root',
+        '/store/relval/CMSSW_3_0_0_pre7/RelValZTT/GEN-SIM-RECO/STARTUP_30X_v1/0006/4CBAC9FC-56E8-DD11-90F5-000423D986C4.root',
+        '/store/relval/CMSSW_3_0_0_pre7/RelValZTT/GEN-SIM-RECO/STARTUP_30X_v1/0006/B0AF7439-57E8-DD11-BC2D-001617C3B77C.root',
+        '/store/relval/CMSSW_3_0_0_pre7/RelValZTT/GEN-SIM-RECO/STARTUP_30X_v1/0006/C6AEDDC0-6AE8-DD11-8381-001D09F231B0.root'
 )
 )
 
@@ -20,6 +20,16 @@ process.DQMStore = cms.Service("DQMStore")
 process.load("Validation.RecoTau.TauTagValidationProducer_cff")
 process.load("Validation.RecoTau.TauTagValidation_cfi")
 process.load("Validation.RecoTau.RelValHistogramEff_cfi")
+
+###################################################################################################
+#
+#    Name of the file that gets all the DQMMonitorElements and saves them, please modify accordingly
+#
+###################################################################################################
+
+process.saveTauEff = cms.EDAnalyzer("DQMSimpleFileSaver",
+  outputFileName = cms.string('CMSSW_3_0_0_pre7_tauGenJets_TEST.root')
+)
                                  
 process.p = cms.Path(
     process.tauGenJetProducer +
