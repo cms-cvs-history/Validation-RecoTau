39dea6babb9d61c5b1560d4192911f8d963bb6c3
Ricardo Vasquez Sierra <sha1-f7e1e97cb11665c6bb054336d8a4a436897d2a02@cern.ch>
2009-03-11 11:17:26 +0000

    Now uses the jet collections that the event contains and can be easily replaced in the cfi file in the python directory.

--- test/TauTagValidationGenJets_cfg.py
+++ test/TauTagValidationGenJets_cfg.py
@@ -1,59 +1,43 @@
-# The following comments couldn't be translated into the new config version:
-
-#module execution use first line for Z->TauTau sample and the second for QCD sample changing accordingly the DataType on the PFTauTagVal module above
-
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("TEST")
 
-#process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+       '/store/relval/CMSSW_3_1_0_pre3/RelValQCD_FlatPt_15_3000/GEN-SIM-DIGI-RECO/IDEAL_30X_FastSim_v1/0001/1AB469A4-520A-DE11-A047-001617E30E28.root',
+        '/store/relval/CMSSW_3_1_0_pre3/RelValQCD_FlatPt_15_3000/GEN-SIM-DIGI-RECO/IDEAL_30X_FastSim_v1/0001/2A091AB5-950A-DE11-B561-000423D9A2AE.root',
+        '/store/relval/CMSSW_3_1_0_pre3/RelValQCD_FlatPt_15_3000/GEN-SIM-DIGI-RECO/IDEAL_30X_FastSim_v1/0001/3EF98F45-770A-DE11-AD4B-000423D944F8.root',
+        '/store/relval/CMSSW_3_1_0_pre3/RelValQCD_FlatPt_15_3000/GEN-SIM-DIGI-RECO/IDEAL_30X_FastSim_v1/0001/9235826C-7D0A-DE11-9762-001617C3B76A.root',
+        '/store/relval/CMSSW_3_1_0_pre3/RelValQCD_FlatPt_15_3000/GEN-SIM-DIGI-RECO/IDEAL_30X_FastSim_v1/0001/C653FC9D-520A-DE11-AF0C-001617C3B6CC.root',
+        '/store/relval/CMSSW_3_1_0_pre3/RelValQCD_FlatPt_15_3000/GEN-SIM-DIGI-RECO/IDEAL_30X_FastSim_v1/0001/D84BEF86-760A-DE11-9FE8-000423D6CA42.root',
+        '/store/relval/CMSSW_3_1_0_pre3/RelValQCD_FlatPt_15_3000/GEN-SIM-DIGI-RECO/IDEAL_30X_FastSim_v1/0001/D84EDCA1-520A-DE11-9159-001617C3B706.root',
+        '/store/relval/CMSSW_3_1_0_pre3/RelValQCD_FlatPt_15_3000/GEN-SIM-DIGI-RECO/IDEAL_30X_FastSim_v1/0001/DC4C12A1-520A-DE11-B96C-001617C3B6DE.root'
+)
+)
 
-process.load("HLTriggerOffline.Tau.Validation.HLTTauReferences_cfi")
-
-process.load("Validation.RecoTau.TauTagValidationGenJets_cfi")
-
-process.load("Validation.RecoTau.GenJetRefProducer_cfi")
-
-process.load("Configuration.StandardSequences.GeometryPilot2_cff")
-process.load("Configuration.StandardSequences.MagneticField_cff")
-process.load("Configuration.StandardSequences.Reconstruction_cff")
-
-# Conditions: fake or frontier
-process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = 'IDEAL_V9::All'
-
-process.load("RecoTauTag.Configuration.RecoPFTauTag_cff")
-process.load("RecoTauTag.Configuration.RecoTauTag_cff")
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
 
 process.DQMStore = cms.Service("DQMStore")
 
-process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring(
-  '/store/relval/CMSSW_2_1_10/RelValZTT/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/IDEAL_V9_v2/0000/3E09F7AB-A699-DD11-B642-000423D94C68.root',
-        '/store/relval/CMSSW_2_1_10/RelValZTT/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/IDEAL_V9_v2/0000/4A07D9D9-AA99-DD11-B646-000423D98DD4.root',
-        '/store/relval/CMSSW_2_1_10/RelValZTT/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/IDEAL_V9_v2/0000/54FE0387-A999-DD11-8ACE-000423D998BA.root',
-        '/store/relval/CMSSW_2_1_10/RelValZTT/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/IDEAL_V9_v2/0000/58EFF32A-A899-DD11-BBD1-000423D991F0.root',
-        '/store/relval/CMSSW_2_1_10/RelValZTT/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/IDEAL_V9_v2/0000/647A6A85-A999-DD11-8159-000423D6CA6E.root',
-        '/store/relval/CMSSW_2_1_10/RelValZTT/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/IDEAL_V9_v2/0000/66A0F94F-AA99-DD11-8DAB-000423D94E1C.root',
-        '/store/relval/CMSSW_2_1_10/RelValZTT/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/IDEAL_V9_v2/0000/74AD18D5-A499-DD11-AE7C-000423D996C8.root',
-        '/store/relval/CMSSW_2_1_10/RelValZTT/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/IDEAL_V9_v2/0000/880BA685-A999-DD11-B3E2-001617DBD472.root',
-        '/store/relval/CMSSW_2_1_10/RelValZTT/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/IDEAL_V9_v2/0000/8A4147D3-AA99-DD11-8F6F-000423D99996.root',
-        '/store/relval/CMSSW_2_1_10/RelValZTT/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/IDEAL_V9_v2/0000/9CB09C57-AA99-DD11-B17B-001617C3B64C.root',
-        '/store/relval/CMSSW_2_1_10/RelValZTT/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/IDEAL_V9_v2/0000/A49D4C86-A999-DD11-BF99-001617C3B5F4.root'
-    )
+process.load("Validation.RecoTau.TauTagValidationGenJets_cfi")
+process.load("Validation.RecoTau.RelValHistogramEff_cfi")
+
+###################################################################################################
+#
+#    Name of the file that gets all the DQMMonitorElements and saves them, please modify accordingly
+#
+###################################################################################################
+
+process.saveTauEff = cms.EDAnalyzer("DQMSimpleFileSaver",
+  outputFileName = cms.string('CMSSW_3_1_0_pre3_RelValQCD_FlatPt_15_3000.root')
 )
+                                 
+process.p = cms.Path(
+#    process.tauGenJetProducer +
+    process.tauTagValidation +
+    process.TauEfficiencies +
+    process.saveTauEff)
 
-process.maxEvents = cms.untracked.PSet( 
-    input = cms.untracked.int32(200)
-)
-
-process.SimpleMemoryCheck = cms.Service("SimpleMemoryCheck",
-    ignoreTotal = cms.untracked.int32(1) ## default is one
-)
-
-process.p = cms.Path(process.PFTauHighEfficiency*
-                     process.GenJetProducer*
-                     process.generatorLevelJets)
-
-#process.p1 = cms.EndPath(process.out)
-
+process.schedule = cms.Schedule(process.p)
