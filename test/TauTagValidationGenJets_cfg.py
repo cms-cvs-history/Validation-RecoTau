514046d7bfee6c00e2930be8d9d890bd6c7b13a9
Ricardo Vasquez Sierra <sha1-f7e1e97cb11665c6bb054336d8a4a436897d2a02@cern.ch>
2009-03-28 18:04:57 +0000

    Added the running of the PFTau id to make sure the it works with the new names of the TauID collections.

--- test/TauTagValidationGenJets_cfg.py
+++ test/TauTagValidationGenJets_cfg.py
@@ -4,25 +4,17 @@ process = cms.Process("TEST")
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-        '/store/relval/CMSSW_3_1_0_pre2/RelValQCD_FlatPt_15_3000/GEN-SIM-DIGI-RECO/IDEAL_30X_FastSim_v1/0001/147F4EAF-AE03-DE11-B98A-000423D6006E.root',
-        '/store/relval/CMSSW_3_1_0_pre2/RelValQCD_FlatPt_15_3000/GEN-SIM-DIGI-RECO/IDEAL_30X_FastSim_v1/0001/24A7D1A3-AE03-DE11-8CFC-000423D99B3E.root',
-        '/store/relval/CMSSW_3_1_0_pre2/RelValQCD_FlatPt_15_3000/GEN-SIM-DIGI-RECO/IDEAL_30X_FastSim_v1/0001/268E6CE8-AE03-DE11-8C29-001D09F231B0.root',
-        '/store/relval/CMSSW_3_1_0_pre2/RelValQCD_FlatPt_15_3000/GEN-SIM-DIGI-RECO/IDEAL_30X_FastSim_v1/0001/3400D4B5-AE03-DE11-8D98-000423D98920.root',
-        '/store/relval/CMSSW_3_1_0_pre2/RelValQCD_FlatPt_15_3000/GEN-SIM-DIGI-RECO/IDEAL_30X_FastSim_v1/0001/568DC4AC-AE03-DE11-9577-000423D8FA38.root',
-        '/store/relval/CMSSW_3_1_0_pre2/RelValQCD_FlatPt_15_3000/GEN-SIM-DIGI-RECO/IDEAL_30X_FastSim_v1/0001/581EB8AD-AE03-DE11-A558-001617E30D52.root',
-        '/store/relval/CMSSW_3_1_0_pre2/RelValQCD_FlatPt_15_3000/GEN-SIM-DIGI-RECO/IDEAL_30X_FastSim_v1/0001/6CDAA1AD-AE03-DE11-85FF-000423D985B0.root',
-        '/store/relval/CMSSW_3_1_0_pre2/RelValQCD_FlatPt_15_3000/GEN-SIM-DIGI-RECO/IDEAL_30X_FastSim_v1/0001/80C385D6-AE03-DE11-8E09-000423D99614.root',
-        '/store/relval/CMSSW_3_1_0_pre2/RelValQCD_FlatPt_15_3000/GEN-SIM-DIGI-RECO/IDEAL_30X_FastSim_v1/0001/982811D3-AE03-DE11-8372-000423D98FBC.root',
-        '/store/relval/CMSSW_3_1_0_pre2/RelValQCD_FlatPt_15_3000/GEN-SIM-DIGI-RECO/IDEAL_30X_FastSim_v1/0001/A81028D8-AE03-DE11-8808-001D09F28EA3.root',
-        '/store/relval/CMSSW_3_1_0_pre2/RelValQCD_FlatPt_15_3000/GEN-SIM-DIGI-RECO/IDEAL_30X_FastSim_v1/0001/B69F9776-DB03-DE11-9E22-000423D98DC4.root',
-        '/store/relval/CMSSW_3_1_0_pre2/RelValQCD_FlatPt_15_3000/GEN-SIM-DIGI-RECO/IDEAL_30X_FastSim_v1/0001/C21A76B1-AE03-DE11-BE6B-001617C3B6DE.root',
-        '/store/relval/CMSSW_3_1_0_pre2/RelValQCD_FlatPt_15_3000/GEN-SIM-DIGI-RECO/IDEAL_30X_FastSim_v1/0001/C679ADB2-AE03-DE11-BF43-001617E30D38.root',
-        '/store/relval/CMSSW_3_1_0_pre2/RelValQCD_FlatPt_15_3000/GEN-SIM-DIGI-RECO/IDEAL_30X_FastSim_v1/0001/FCA230D0-AE03-DE11-8999-001617C3B654.root'
+        '/store/relval/CMSSW_3_1_0_pre4/RelValQCD_FlatPt_15_3000/GEN-SIM-DIGI-RECO/IDEAL_30X_FastSim_v1/0003/00DD82EE-0616-DE11-ABAD-000423D6B42C.root',
+        '/store/relval/CMSSW_3_1_0_pre4/RelValQCD_FlatPt_15_3000/GEN-SIM-DIGI-RECO/IDEAL_30X_FastSim_v1/0003/1A3FDC02-0916-DE11-9DC7-001617E30D0A.root',
+        '/store/relval/CMSSW_3_1_0_pre4/RelValQCD_FlatPt_15_3000/GEN-SIM-DIGI-RECO/IDEAL_30X_FastSim_v1/0003/1C94B51A-AC16-DE11-8F94-001617C3B76E.root',
+        '/store/relval/CMSSW_3_1_0_pre4/RelValQCD_FlatPt_15_3000/GEN-SIM-DIGI-RECO/IDEAL_30X_FastSim_v1/0003/364F41D9-0516-DE11-BF0A-000423D9853C.root',
+        '/store/relval/CMSSW_3_1_0_pre4/RelValQCD_FlatPt_15_3000/GEN-SIM-DIGI-RECO/IDEAL_30X_FastSim_v1/0003/3E1BCACC-0516-DE11-B6DC-000423D6A6F4.root',
+        '/store/relval/CMSSW_3_1_0_pre4/RelValQCD_FlatPt_15_3000/GEN-SIM-DIGI-RECO/IDEAL_30X_FastSim_v1/0003/5688EBA3-0516-DE11-8170-000423D94AA8.root'
 )
 )
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(100)
+    input = cms.untracked.int32(-1)
 )
 
 process.DQMStore = cms.Service("DQMStore")
@@ -30,6 +22,9 @@ process.DQMStore = cms.Service("DQMStore")
 process.load("PhysicsTools.JetMCAlgos.GenJetsSelector_iterativeCone5GenJets_cfi")
 process.load("Validation.RecoTau.TauTagValidationGenJets_cfi")
 process.load("Validation.RecoTau.RelValHistogramEff_cfi")
+process.load("RecoTauTag.Configuration.RecoPFTauTag_cff")
+process.load("FastSimulation.Configuration.CommonInputs_cff")
+process.load("Configuration.StandardSequences.MagneticField_38T_cff")
 
 ###################################################################################################
 #
@@ -38,10 +33,11 @@ process.load("Validation.RecoTau.RelValHistogramEff_cfi")
 ###################################################################################################
 
 process.saveTauEff = cms.EDAnalyzer("DQMSimpleFileSaver",
-  outputFileName = cms.string('CMSSW_3_1_0_pre2_RelValQCD_FlatPt_15_3000_100.root')
+  outputFileName = cms.string('CMSSW_3_1_0_pre4_RelValQCD_FlatPt_15_3000_100.root')
 )
                                  
 process.p = cms.Path(
+    process.PFTau+
     process.iterativeCone5GenJetsEta25 +
     process.iterativeCone5GenJetsPt5Cumulative +
     process.tauTagValidation +
