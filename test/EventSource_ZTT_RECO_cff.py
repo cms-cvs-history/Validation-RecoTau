2f12fccff08b392aaaa2ee61962761fd8df0b6da
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2009-06-29 22:12:06 +0000

    Remove genParticles, add $PastResults enviro variable.

--- test/EventSource_ZTT_RECO_cff.py
+++ test/EventSource_ZTT_RECO_cff.py
@@ -5,15 +5,9 @@ readFiles = cms.untracked.vstring()
 secFiles = cms.untracked.vstring() 
 source = cms.Source ("PoolSource",fileNames = readFiles, secondaryFileNames = secFiles)
 readFiles.extend( [
-   '/store/relval/CMSSW_3_1_0_pre10/RelValZTT/GEN-SIM-RECO/STARTUP_31X_v1/0009/0EC0E0FE-0558-DE11-986D-001D09F29146.root',
-   '/store/relval/CMSSW_3_1_0_pre10/RelValZTT/GEN-SIM-RECO/STARTUP_31X_v1/0008/969D21C1-E857-DE11-A00D-001D09F23D04.root',
-   '/store/relval/CMSSW_3_1_0_pre10/RelValZTT/GEN-SIM-RECO/STARTUP_31X_v1/0008/840866A4-EA57-DE11-8A75-001D09F28EC1.root',
-   '/store/relval/CMSSW_3_1_0_pre10/RelValZTT/GEN-SIM-RECO/STARTUP_31X_v1/0008/70EDDE9C-E957-DE11-A16D-000423D98868.root',
-   '/store/relval/CMSSW_3_1_0_pre10/RelValZTT/GEN-SIM-RECO/STARTUP_31X_v1/0001/CCE6F244-1458-DE11-A5E5-001A92971B5E.root',
-   '/store/relval/CMSSW_3_1_0_pre10/RelValZTT/GEN-SIM-RECO/STARTUP_31X_v1/0001/C4E56D8D-075A-DE11-B5E7-001A92971B54.root',
-   '/store/relval/CMSSW_3_1_0_pre10/RelValZTT/GEN-SIM-RECO/STARTUP_31X_v1/0001/16E3E391-7059-DE11-9887-001A928116F4.root' ] );
-
-
+   '/store/relval/CMSSW_3_1_0_pre11/RelValZTT/GEN-SIM-RECO/STARTUP31X_V1-v1/0000/6C9BEF7F-AD64-DE11-AE61-001D09F23A6B.root',
+   '/store/relval/CMSSW_3_1_0_pre11/RelValZTT/GEN-SIM-RECO/STARTUP31X_V1-v1/0000/62DD9EFC-7164-DE11-B38D-0019B9F72BFF.root',
+   '/store/relval/CMSSW_3_1_0_pre11/RelValZTT/GEN-SIM-RECO/STARTUP31X_V1-v1/0000/5E611C51-9864-DE11-A276-001D09F24DA8.root' ] );
 
 secFiles.extend( [
    ] )
