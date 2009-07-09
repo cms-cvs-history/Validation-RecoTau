bc1b9b9392210d79df5f6e0172f8bc588afddd16
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2009-07-09 18:20:15 +0000

    3_1_1(!) relvals

--- test/EventSource_ZTT_RECO_cff.py
+++ test/EventSource_ZTT_RECO_cff.py
@@ -5,9 +5,11 @@ readFiles = cms.untracked.vstring()
 secFiles = cms.untracked.vstring() 
 source = cms.Source ("PoolSource",fileNames = readFiles, secondaryFileNames = secFiles)
 readFiles.extend( [
-   '/store/relval/CMSSW_3_1_0_pre11/RelValZTT/GEN-SIM-RECO/STARTUP31X_V1-v1/0000/6C9BEF7F-AD64-DE11-AE61-001D09F23A6B.root',
-   '/store/relval/CMSSW_3_1_0_pre11/RelValZTT/GEN-SIM-RECO/STARTUP31X_V1-v1/0000/62DD9EFC-7164-DE11-B38D-0019B9F72BFF.root',
-   '/store/relval/CMSSW_3_1_0_pre11/RelValZTT/GEN-SIM-RECO/STARTUP31X_V1-v1/0000/5E611C51-9864-DE11-A276-001D09F24DA8.root' ] );
+   '/store/relval/CMSSW_3_1_1/RelValZTT/GEN-SIM-RECO/STARTUP31X_V1-v2/0003/DAAF1151-EF6B-DE11-B09A-000423D6A6F4.root',
+   '/store/relval/CMSSW_3_1_1/RelValZTT/GEN-SIM-RECO/STARTUP31X_V1-v2/0002/98101275-E36B-DE11-9E7A-001D09F2A465.root',
+   '/store/relval/CMSSW_3_1_1/RelValZTT/GEN-SIM-RECO/STARTUP31X_V1-v2/0002/7481E874-E36B-DE11-AEC1-0019B9F72BFF.root',
+   '/store/relval/CMSSW_3_1_1/RelValZTT/GEN-SIM-RECO/STARTUP31X_V1-v2/0002/2C6D327D-E36B-DE11-9E9B-000423D98844.root' ] );
+
 
 secFiles.extend( [
    ] )
