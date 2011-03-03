5e88f9dbcc6682922bd7af7278c73eea26d78c94
cmsbuild <>
2011-03-03 10:22:15 +0000

    This commit was manufactured by cvs2git to create tag 'Validation-RecoTau- MV-03-03-11P'.
    Sprout from master 2011-03-03 10:22:13 UTC Mauro Verzetti <sha1-a72272ea14fe5ac21105bbc72f1ae068f99bb5e2@cern.ch> 'CRAB cfg file to run the validation (to be modified by the user)'
    Cherrypick from master 2010-03-11 00:18:48 UTC Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch> 'Change PFTauAnalyzer to support interface change in PFTauElementOperators...':
        interface/TauTagValidation.h
        python/ProduceFullSimAndDigisForTaus_cff.py
        python/ValidateTausOnQCD_cff.py
        python/ValidateTausOnZEE_cff.py
        python/ValidateTausOnZMM_cff.py
        python/ValidateTausOnZTT_cff.py
        test/EventSource_ZEE_RECO_cff.py
        test/EventSource_ZTT_RECO_cff.py
        test/PFTauAnalyzer.cc
    Delete:
        python/ValidateTausOnRealData_cff.py
        python/steering.py
        python/tools/__init__.py
        python/tools/embedder.py
        python/tools/matcher.py
        python/tools/prototypes.py
        python/tools/validator.py
        test/EventSource_QCD_RECO_cff.py

--- test/EventSource_ZTT_RECO_cff.py
+++ test/EventSource_ZTT_RECO_cff.py
@@ -5,10 +5,6 @@ readFiles = cms.untracked.vstring()
 secFiles = cms.untracked.vstring() 
 source = cms.Source ("PoolSource",fileNames = readFiles, secondaryFileNames = secFiles)
 readFiles.extend( [
-<<<<<<< EventSource_ZTT_RECO_cff.py
-    #Source input files to analyze
-      ] )
-=======
     '/store/relval/CMSSW_3_6_0_pre2/RelValZTT/GEN-SIM-RECO/START3X_V24-v1/0001/C27387A2-6F27-DF11-A4D5-002618943880.root',
     '/store/relval/CMSSW_3_6_0_pre2/RelValZTT/GEN-SIM-RECO/START3X_V24-v1/0000/EE23E60D-1A27-DF11-AD51-002618943920.root',
     '/store/relval/CMSSW_3_6_0_pre2/RelValZTT/GEN-SIM-RECO/START3X_V24-v1/0000/2AB69378-1A27-DF11-BBB4-002354EF3BDA.root',
@@ -16,7 +12,6 @@ readFiles.extend( [
     '/store/relval/CMSSW_3_6_0_pre2/RelValZTT/GEN-SIM-RECO/START3X_V24-v1/0000/1C3D776D-1927-DF11-A6FC-002618943894.root' 
 ] )
 
->>>>>>> 1.14
 
 
 secFiles.extend( [
