a0d6825015016a4b516cd8850eb074ab5d47ed1a
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2011-03-06 08:10:28 +0000

    Adding test case for TMVA

--- test/EventSource_ZTT_RECO_cff.py
+++ test/EventSource_ZTT_RECO_cff.py
@@ -5,20 +5,11 @@ readFiles = cms.untracked.vstring()
 secFiles = cms.untracked.vstring() 
 source = cms.Source ("PoolSource",fileNames = readFiles, secondaryFileNames = secFiles)
 readFiles.extend( [
-<<<<<<< EventSource_ZTT_RECO_cff.py
-    #Source input files to analyze
-      ] )
-=======
-    '/store/relval/CMSSW_3_6_0_pre2/RelValZTT/GEN-SIM-RECO/START3X_V24-v1/0001/C27387A2-6F27-DF11-A4D5-002618943880.root',
-    '/store/relval/CMSSW_3_6_0_pre2/RelValZTT/GEN-SIM-RECO/START3X_V24-v1/0000/EE23E60D-1A27-DF11-AD51-002618943920.root',
-    '/store/relval/CMSSW_3_6_0_pre2/RelValZTT/GEN-SIM-RECO/START3X_V24-v1/0000/2AB69378-1A27-DF11-BBB4-002354EF3BDA.root',
-    '/store/relval/CMSSW_3_6_0_pre2/RelValZTT/GEN-SIM-RECO/START3X_V24-v1/0000/22EC2405-1A27-DF11-8F83-002618943943.root',
-    '/store/relval/CMSSW_3_6_0_pre2/RelValZTT/GEN-SIM-RECO/START3X_V24-v1/0000/1C3D776D-1927-DF11-A6FC-002618943894.root' 
+   '/store/relval/CMSSW_4_2_0_pre6/RelValZTT/GEN-SIM-RECO/START42_V4-v1/0023/267CF021-A645-E011-A05B-00261894391D.root',
+   '/store/relval/CMSSW_4_2_0_pre6/RelValZTT/GEN-SIM-RECO/START42_V4-v1/0020/52137829-5945-E011-B764-00261894383F.root',
+   '/store/relval/CMSSW_4_2_0_pre6/RelValZTT/GEN-SIM-RECO/START42_V4-v1/0020/24E484D7-5A45-E011-8B00-00261894394B.root'
 ] )
-
->>>>>>> 1.14
 
 
 secFiles.extend( [
    ] )
-
