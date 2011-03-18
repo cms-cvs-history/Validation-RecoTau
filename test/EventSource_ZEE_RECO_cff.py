6ff6c29d4feadf67319bed74d081295c375f7570
cmsbuild <>
2011-03-18 16:20:37 +0000

    This commit was manufactured by cvs2git to create tag 'Validation-RecoTau- CMSSW_4_2_0_pre8'.
    Sprout from master 2011-03-18 16:20:35 UTC Mauro Verzetti <sha1-a72272ea14fe5ac21105bbc72f1ae068f99bb5e2@cern.ch> 'Forced to run even in presence of different tag version'
    Cherrypick from master 2010-03-11 00:18:48 UTC Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch> 'Change PFTauAnalyzer to support interface change in PFTauElementOperators...':
        interface/TauTagValidation.h
        python/ValidateTausOnQCD_cff.py
        python/ValidateTausOnZEE_cff.py
        python/ValidateTausOnZMM_cff.py
        python/ValidateTausOnZTT_cff.py
        test/EventSource_ZEE_RECO_cff.py
        test/EventSource_ZTT_RECO_cff.py
        test/PFTauAnalyzer.cc
    Delete:
        python/ValidateTausOnRealData_cff.py
        python/steering.py
        python/tools/__init__.py
        python/tools/embedder.py
        python/tools/matcher.py
        python/tools/prototypes.py
        python/tools/validator.py
        test/EventSource_QCD_RECO_cff.py

--- test/EventSource_ZEE_RECO_cff.py
+++ test/EventSource_ZEE_RECO_cff.py
@@ -5,14 +5,12 @@ readFiles = cms.untracked.vstring()
 secFiles = cms.untracked.vstring() 
 source = cms.Source ("PoolSource",fileNames = readFiles, secondaryFileNames = secFiles)
 readFiles.extend( [
-        'file:./Samples/RelValZEE/12F886E5-5A1B-DF11-8625-001A928116E0.root',
-        'file:./Samples/RelValZEE/644D54F3-551B-DF11-892A-0018F3D09708.root',
-        'file:./Samples/RelValZEE/6A421747-4E1B-DF11-B902-0018F34D0D62.root',
-        'file:./Samples/RelValZEE/701D60CC-531B-DF11-955A-001731AF6B85.root',
-        'file:./Samples/RelValZEE/D8886291-541B-DF11-8BC0-003048678ADA.root'
-      ] )
-
+   '/store/relval/CMSSW_3_1_1/RelValZEE/GEN-SIM-RECO/MC_31X_V2-v1/0002/FC71916C-756B-DE11-8631-000423D94700.root',
+   '/store/relval/CMSSW_3_1_1/RelValZEE/GEN-SIM-RECO/MC_31X_V2-v1/0002/B672A1C5-746B-DE11-93A8-000423D944F0.root',
+   '/store/relval/CMSSW_3_1_1/RelValZEE/GEN-SIM-RECO/MC_31X_V2-v1/0002/5A0F871C-756B-DE11-BFE1-001D09F2545B.root',
+   '/store/relval/CMSSW_3_1_1/RelValZEE/GEN-SIM-RECO/MC_31X_V2-v1/0002/44507D17-D66B-DE11-A165-000423D94524.root' ] );
 
 secFiles.extend( [
    ] )
+
 
