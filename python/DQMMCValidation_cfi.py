f6acf72df0e2240b0f5bc2ff763597a796cca89e
Mauro Verzetti <sha1-a72272ea14fe5ac21105bbc72f1ae068f99bb5e2@cern.ch>
2012-02-02 17:22:17 +0000

    major code rewriting to allow multiple sequences and easier cloning. New Sequences

--- python/DQMMCValidation_cfi.py
+++ python/DQMMCValidation_cfi.py
@@ -1,119 +1,46 @@
-import FWCore.ParameterSet.Config as cms
-#import Validation.RecoTau.ValidateTausOnZTT_cff as zttVal
-from Validation.RecoTau.dataTypes.ValidateTausOnZTT_cff import *
-import copy
-import Validation.RecoTau.ValidationUtils as Utils
-
-#------------------------------------------------------------
-#                     Producing Num e Denom
-#------------------------------------------------------------
-
-def PrintSeq(seq, tau=False):
-    scanner = Utils.Scanner()
-    seq.visit(scanner)
-    for module in scanner.modules():
-        print type(module)
-        if type(module) is cms.EDAnalyzer and tau:# or type(module) is cms.EDFilter:
-            print module.TauProducer.value() + module.ExtensionName.value()
-
-def SetSignalPars(module):
-    module.ExtensionName = (module.ExtensionName.value()+"_Signal")
-    module.RefCollection = "zttKinemSelection"
-
-def SetFakePars(module):
-    module.ExtensionName = (module.ExtensionName.value()+"_Fakes")
-    module.RefCollection = "qcdKinemSelection"
-
-pfTauRunDQMValidation = cms.Sequence()
-
-tauGenJets = copy.deepcopy(tauGenJets)
-zttDenominator = objectTypeSelectedTauValDenominatorModule.clone()
-zttKinemSelection= kinematicSelectedTauValDenominator.clone(src = cms.InputTag("zttDenominator"))
-
-zttModifier = ApplyFunctionToSequence(SetSignalPars)
-TauValNumeratorAndDenominator.visit(zttModifier)        
-pfTauRunDQMValidation += TauValNumeratorAndDenominator
-
 from Validation.RecoTau.dataTypes.ValidateTausOnQCD_cff import *
+from Validation.RecoTau.dataTypes.ValidateTausOnRealData_cff import *
+from Validation.RecoTau.dataTypes.ValidateTausOnRealElectronsData_cff import *
+from Validation.RecoTau.dataTypes.ValidateTausOnRealMuonsData_cff import *
+from Validation.RecoTau.dataTypes.ValidateTausOnZEEFastSim_cff import *
+from Validation.RecoTau.dataTypes.ValidateTausOnZEE_cff import *
+from Validation.RecoTau.dataTypes.ValidateTausOnZMM_cff import *
+from Validation.RecoTau.dataTypes.ValidateTausOnZTTFastSim_cff import *
+from Validation.RecoTau.dataTypes.ValidateTausOnZTT_cff import *
 
-genParticlesForJetsQCD= genParticlesForJets.clone()
-qcdDenominator = objectTypeSelectedTauValDenominatorModule.clone()
-qcdKinemSelection= kinematicSelectedTauValDenominator.clone(src = cms.InputTag("qcdDenominator"))
-
-qcdModifier = ApplyFunctionToSequence(SetFakePars)
-TauValNumeratorAndDenominator2.visit(qcdModifier)        
-pfTauRunDQMValidation += TauValNumeratorAndDenominator2
-
-produceDenoms = cms.Sequence(
-    tauGenJets
-    *zttDenominator
-    *zttKinemSelection
-    +genParticlesForJets
-    *qcdDenominator
-    *qcdKinemSelection
+pfTauRunDQMValidation = cms.Sequence(
+    TauValNumeratorAndDenominatorQCD+
+    TauValNumeratorAndDenominatorRealData+
+    TauValNumeratorAndDenominatorRealElectronsData+
+    TauValNumeratorAndDenominatorRealMuonsData+
+    TauValNumeratorAndDenominatorZEEFastSim+
+    TauValNumeratorAndDenominatorZEE+
+    TauValNumeratorAndDenominatorZMM+
+    TauValNumeratorAndDenominatorZTTFastSim+
+    TauValNumeratorAndDenominatorZTT
     )
 
-#-------------------------------------------------------------------------------------------------------
-#                     Producing Efficiencies (postValidation)
-#-------------------------------------------------------------------------------------------------------
+produceDenoms = cms.Sequence(
+    produceDenominatorQCD+
+    produceDenominatorRealData+
+    produceDenominatorRealElectronsData+
+    produceDenominatorRealMuonsData+
+    produceDenominatorZEEFastSim+
+    produceDenominatorZEE+
+    produceDenominatorZMM+
+    produceDenominatorZTTFastSim+
+    produceDenominatorZTT
+    )
 
+runTauEff = cms.Sequence(
+    TauEfficienciesQCD+
+    TauEfficienciesRealData+
+    TauEfficienciesRealElectronsData+
+    TauEfficienciesRealMuonsData+
+    TauEfficienciesZEEFastSim+
+    TauEfficienciesZEE+
+    TauEfficienciesZMM+
+    TauEfficienciesZTTFastSim+
+    TauEfficienciesZTT
+    )
 
-plotPsetSignal = Utils.SetPlotSequence(TauValNumeratorAndDenominator)
-plotPsetFake = Utils.SetPlotSequence(TauValNumeratorAndDenominator2)
-
-efficienciesFake = efficiencies.clone(plots = plotPsetFake)
-TauEfficienciesFake = cms.Sequence(
-   efficienciesFake*
-   normalizePlots
-)
-efficiencies.plots = plotPsetSignal
-TauEfficiencies = cms.Sequence(
-   efficiencies*
-   normalizePlots
-)
-
-runTauEff = cms.Sequence(TauEfficienciesFake + TauEfficiencies)
-
-#--------------------------------------------------------------------------
-#         Making histograms look nicer (not working yet)
-#--------------------------------------------------------------------------
-
-def SetSignalPlotSet(module):
-    module.PrintToFile = False
-    del module.drawJobs.TauIdEffStepByStep
-    for subsetName in module.drawJobs.parameterNames_():
-        subset = getattr(module.drawJobs,subsetName)
-        if hasattr(subset,'plots'):
-            mEs = []
-            for monitorEl in subset.plots.dqmMonitorElements:
-                correcectME = monitorEl[13:]
-                lastUnderscore = correcectME.rfind('_',0,correcectME.rfind('/'))
-                correcectME = correcectME[:lastUnderscore]+'_Signal'+correcectME[lastUnderscore:]
-                mEs.append(correcectME)
-            subset.plots.dqmMonitorElements = cms.vstring(mEs)
-
-
-def SetFakePlotSet(module):
-    module.PrintToFile = False
-    del module.drawJobs.TauIdEffStepByStep
-    for subsetName in module.drawJobs.parameterNames_():
-        subset = getattr(module.drawJobs,subsetName)
-        if hasattr(subset,'plots'):
-#            subset.drawOptionSet = 'fakeRate'
-            subset.yAxis = 'fakeRate'
-#            subset.legend = 'fakeRate'
-            mEs = []
-            for monitorEl in subset.plots.dqmMonitorElements:
-                correcectME = monitorEl[13:]
-                lastUnderscore = correcectME.rfind('_',0,correcectME.rfind('/'))
-                correcectME = correcectME[:lastUnderscore]+'_Fakes'+correcectME[lastUnderscore:]
-                mEs.append(correcectME)
-            subset.plots.dqmMonitorElements = cms.vstring(mEs)
-
-zttModifier = ApplyFunctionToSequence(SetSignalPlotSet)
-plotTauValidation.visit(zttModifier)
-
-qcdModifier = ApplyFunctionToSequence(SetFakePlotSet)
-plotTauValidation2.visit(qcdModifier)        
-
-makeBetterPlots = cms.Sequence(plotTauValidation+plotTauValidation2)
