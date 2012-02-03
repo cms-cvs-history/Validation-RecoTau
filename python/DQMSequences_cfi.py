915bc5c9a7597c4fafee37419f33cd6d58f1ccd9
Mauro Verzetti <sha1-a72272ea14fe5ac21105bbc72f1ae068f99bb5e2@cern.ch>
2012-02-03 09:13:40 +0000

    minor bugfix

--- python/DQMSequences_cfi.py
+++ python/DQMSequences_cfi.py
@@ -1,18 +1,11 @@
-from Validation.RecoTau.dataTypes.ValidateTausOnQCD_cff import *
 from Validation.RecoTau.dataTypes.ValidateTausOnRealData_cff import *
 from Validation.RecoTau.dataTypes.ValidateTausOnRealElectronsData_cff import *
 from Validation.RecoTau.dataTypes.ValidateTausOnRealMuonsData_cff import *
-from Validation.RecoTau.dataTypes.ValidateTausOnZEEFastSim_cff import *
-from Validation.RecoTau.dataTypes.ValidateTausOnZEE_cff import *
-from Validation.RecoTau.dataTypes.ValidateTausOnZMM_cff import *
-from Validation.RecoTau.dataTypes.ValidateTausOnZTTFastSim_cff import *
-from Validation.RecoTau.dataTypes.ValidateTausOnZTT_cff import *
 
 pfTauRunDQMValidation = cms.Sequence(
     TauValNumeratorAndDenominatorRealData+
     TauValNumeratorAndDenominatorRealElectronsData+
-    TauValNumeratorAndDenominatorRealMuonsData+
-    TauValNumeratorAndDenominatorZTT
+    TauValNumeratorAndDenominatorRealMuonsData
 )
 
 produceDenoms = cms.Sequence(
